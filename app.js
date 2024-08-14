f8c09b2944db48c71c1bbfab288e2c96f59df858
Chris <chris79gp@outlook.com>
2024-08-14 21:27:54 +0100

    Build: add MVC dirs and files for `index` and `new` endpoints

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+const app = express();
+
+app.use("/", indexRouter);
+app.use("/new", newRouter);
+
+const PORT = 3000;
+app.listen(PORT => console.log(`server listening on port ${PORT}`));
