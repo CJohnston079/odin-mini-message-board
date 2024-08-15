865d044b9ec52e492c3e8e63bac2f70bc8badecd
Chris <chris79gp@outlook.com>
2024-08-15 16:29:32 +0100

    Feat: add views for `"./"` and `"./new"` routes

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 const app = express();
 
-app.use("/", indexRouter);
-app.use("/new", newRouter);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("index", {});
+});
+
+app.get("/new", (req, res) => {
+	res.render("new", {});
+});
+
+// app.use("/", indexRouter);
+// app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
