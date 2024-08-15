d87eb33f14977eb27efab44204bd577639098858
Chris <chris79gp@outlook.com>
2024-08-15 17:42:21 +0100

    Refactor: move routing logic to `./routes/<route>.js`

--- app.js
+++ app.js
@@ -7,16 +7,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	res.render("index", {});
-});
-
-app.get("/new", (req, res) => {
-	res.render("new", {});
-});
-
-// app.use("/", indexRouter);
-// app.use("/new", newRouter);
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
