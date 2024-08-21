f9434a7b6630c39e5f6a23a41a52d6902946fae3
Chris <chris79gp@outlook.com>
2024-08-21 14:38:14 +0100

    Refactor: move `POST` request for `/new` endpoint from `indexRouter.js` to `newRouter.js`

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,10 +4,4 @@ const messages = require("../models/Messages");
 
 router.get("/", (req, res) => res.render("index", { messages: messages.getAllMessages() }));
 
-router.post("/new", (req, res) => {
-	const { user, text } = req.body;
-	messages.addMessage(user, text);
-	res.redirect("/");
-});
-
 module.exports = router;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const messages = require("../models/Messages");
 
 router.get("/", (req, res) => res.render("new", {}));
+
+router.post("/", (req, res) => {
+	const { user, text } = req.body;
+	messages.addMessage(user, text);
+	res.redirect("/");
+});
 
 module.exports = router;
