d8b5da761e31ee6d8cf99c8942604e21cbaea442
Chris <chris79gp@outlook.com>
2024-08-27 21:58:47 +0100

    Chore: remove redundant `models/Messages.js` file and directory

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,10 +1,10 @@
-const messages = require("../models/Messages");
+// const messages = require("../models/Messages");
 
 const getNew = (req, res) => res.render("new", {});
 
 const postNew = (req, res) => {
 	const { user, text } = req.body;
-	messages.addMessage(user, text);
+	// messages.addMessage(user, text);
 	res.redirect("/");
 };
 
