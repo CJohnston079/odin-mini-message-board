48055b47b8913ca49be9bdebbf9e183b4c683d3a
Chris <chris79gp@outlook.com>
2024-08-28 18:08:04 +0100

    Feat: add `renderMessageColours()` IIFE to `script.js` to randomise colours of message border and user

--- public/script.js
+++ public/script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
 		console.log("No container found");
 	}
 });
+
+renderMessageColours = (function () {
+	const messageContainers = document.querySelectorAll(".message-container");
+
+	const generateRandomColour = () => `hsl(${Math.floor(Math.random() * 360)}, 60%, 50%)`;
+
+	messageContainers.forEach(container => {
+		const colour = generateRandomColour();
+		container.style.borderLeftColor = colour;
+		container.style.color = colour;
+	});
+})();
