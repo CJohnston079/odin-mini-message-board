6a5b497368f0a6b0e7d791e6a440661fa642949f
Chris <chris79gp@outlook.com>
2024-09-04 15:11:09 +0100

    Feat: set `ssl.require: true` for production environments

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = new Pool({
 	host: process.env.HOST,
 	user: process.env.USER,
 	database: process.env.DATABASE,
 	password: process.env.PASSWORD,
 	port: 5432,
+	ssl: isProduction
+		? {
+				rejectUnauthorized: false,
+				require: true,
+		  }
+		: false,
 });
