f4af0e699a08bd0d3f413a38c1cedc4e62c29fcc
Chris <chris79gp@outlook.com>
2024-09-10 14:24:13 +0100

    Chore: remove redundant code to show newest messages first in `script.js`

--- public/script.js
+++ public/script.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const container = document.querySelector("#message-board-container");
-	if (container) {
-		container.scrollTop = container.scrollHeight;
-	} else {
-		console.log("No container found");
-	}
-});
-
-renderMessageColours = (function () {
+const renderMessageColours = (function () {
 	const messageContainers = document.querySelectorAll(".message-container");
 
 	const generateRandomColour = () => `hsl(${Math.floor(Math.random() * 360)}, 60%, 50%)`;
